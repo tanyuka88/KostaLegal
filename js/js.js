5e3bdb08b2077324ca94f9f41cb47ae4010c1711
tanya <tatyana.chen.88@gmail.com>
2015-10-14 22:58:47 +0500

    js.js

---
+++ js/js.js
@@ -0,0 +1,38 @@
+﻿jQuery(document).ready(function($){
+    if($(window).width()>801) {
+        $("#owl-demo").owlCarousel({
+            items : 1,
+            itemsDesktop : [1200,1],
+            itemsDesktopSmall : [800,1],
+            itemsTablet: [500,1],
+            itemsMobile : false,
+            autoPlay: false,
+            speed: 3000,
+            loop: true,
+            navigation: true
+        });
+    }
+    else{
+        $("#owl-demo").owlCarousel({
+            items : 1,
+            itemsDesktop : [1200,1],
+            itemsDesktopSmall : [800,1],
+            itemsTablet: [500,1],
+            itemsMobile : false,
+            autoPlay: false,
+            speed: 3000,
+            loop: true,
+            navigation: false
+        });
+    }
+    var touch 	= $('#touch-menu');
+    var menu 	= $('.menu');
+    $(touch).click(function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+
+});
+
+
